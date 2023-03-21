f31f6f87cbfcdcd58abff59823af7f31f3496175
zee-ham-su <hsufiian@yahoo.com>
2023-03-21 02:07:06 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * main - output _putchar, with a new line afterwards
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+write(1, "_putchar\n", 9);
+
+return(0);
+}
