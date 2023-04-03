d521a4c59a7c4b8733738cd2e0e93fec8cf10af4
zee-ham-su <hsufiian@yahoo.com>
2023-04-03 16:27:28 -0700

    summation diagonally

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_diagsums - to print the sum of two numbers
+ *@size: matrix size
+ *@a: input pointer
+ *Return: zilch.
+ */
+
+void print_diagsums(int *a, int size)
+{
+int i;
+int j;
+int sum8 = 0;
+int sum9 = 0;
+
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (i == j)
+sum8 += a[i * size + j];
+if (i + j == size - 1)
+sum9 += a[i * size + j];
+}
+}
+printf("%d, %d\n", sum8, sum9);
+}
