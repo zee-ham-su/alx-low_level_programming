cb058f183ed943626def352e0d76a9c1e6722427
zee-ham-su <hsufiian@yahoo.com>
2023-04-03 15:28:28 -0700

    search for a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ *_strpbrk - search a string for any of a set of bytes
+ *@accept: string number 2
+ *@s: string number 1
+ *Return:  Returns the length of the initial segment...
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i;
+char *p;
+p = s;
+while (*p)
+{
+for (i = 0; accept[i]; i++)
+{
+if (*p == accept[i])
+{
+return (p);
+}
+}
+p++;
+}
+return (NULL);
+}
