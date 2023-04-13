689858e4bd97c8763a05b5b12bf6d6dae478894a
zee-ham-su <hsufiian@yahoo.com>
2023-04-13 08:53:41 -0700

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -4,28 +4,29 @@
 
 
 /**
- *string_nconcat - concatenates n bytes of a string to another string
- * @s1: string to append to
- * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
+ * string_nconcat - Concatenates n bytes of a string to another string
+ * @s1: The first string
+ * @s2: The second string to concatenate from
+ * @n: The number of bytes from s2 to concatenate to s1
  *
- * Return: pointer to the resulting string
+ * Return: Pointer to the resulting string
  */
-int str_len(char *s);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
 char *p;
 unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
+
 
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
 
+
 len1 = str_len(s1);
 len2 = str_len(s2);
+
 
 if (n < len2)
 p = malloc(sizeof(char) * (len1 + n + 1));
@@ -38,8 +39,26 @@ return (NULL);
 for (i = 0; i < len1; i++)
 p[i] = s1[i];
 
-for (j = 0; j < len2; j++)
-p[len1 + i] = s2[i];
+for (j = 0; j < n && s2[j] != '\0'; j++)
+p[len1 + j] = s2[j];
+
+p[len1 + j] = '\0';
 
 return (p);
+}
+
+/**
+ * str_len - Returns the length of a string
+ * @s: The string to find the length of
+ *
+ * Return: The length of the string
+ */
+unsigned int str_len(char *s)
+{
+int len = 0;
+
+while (s[len] != '\0')
+len++;
+
+return (len);
 }
