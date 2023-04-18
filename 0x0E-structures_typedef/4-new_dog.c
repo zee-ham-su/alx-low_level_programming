7d2df6fb878ab0bec2bab70e1f271d644aa388a2
zee-ham-su <hsufiian@yahoo.com>
2023-04-17 22:10:15 -0700

    nouveau chien

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,55 +1,50 @@
-#include <stdio.h>
-#include "dog.h"
 #include <stdlib.h>
 #include <string.h>
-
+#include "dog.h"
 
 /**
- * new_dog - Creates a new dog_t structure
- * @name:dog name
- * @age: dog age
- * @owner: dog owner
+ * new_dog - creates a new dog.
+ * @name: name of the dog.
+ * @age: age of the dog.
+ * @owner: owner of the dog.
  *
- * Return: A pointer to the new dog_t structure, or NULL if memory fails
+ * Return: pointer to struct dog.
+ * If fails, returns NULL.
  */
-
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
-
+size_t name_len, owner_len;
 dog_t *dog;
-dog = malloc(sizeof(dog_t));
-if (dog == NULL)
+
+
+if (!name || !owner)
 return (NULL);
 
-dog->name = malloc(strlen(name) + 1);
+name_len = strlen(name) + 1;
+owner_len = strlen(owner) + 1;
 
-if (dog->name == NULL)
+dog = malloc(sizeof(*dog));
+if (!dog)
+return (NULL);
+
+dog->name = malloc(name_len);
+if (!dog->name)
 {
 free(dog);
 return (NULL);
 }
 
-for (size_t i = 0; i <= strlen(name); i++)
-dog->name[i] = name[i];
-
-dog->owner = malloc(strlen(owner) + 1);
-
-if (dog->owner == NULL)
+dog->owner = malloc(owner_len);
+if (!dog->owner)
 {
 free(dog->name);
 free(dog);
 return (NULL);
 }
 
-for (size_t i = 0; i <= strlen(owner); i++)
-dog->owner[i] = owner[i];
-
-
-
+memcpy(dog->name, name, name_len);
 dog->age = age;
-
+memcpy(dog->owner, owner, owner_len);
 
 return (dog);
 }
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+/**
+ * free_dog - frees dogs.
+ * @d: struct dog.
+ *
+ * Return: no return.
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+printf("Error: the dog pointer is NULL.\n");
+return;
+}
+    
+free(d->owner);
+free(d->name);
+free(d);
+}
