8d16a5ebe01ada18980b8b845eaa37a806c286a9
zee-ham-su <hsufiian@yahoo.com>
2023-04-17 22:16:02 -0700

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,9 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 /**
  * free_dog - frees dogs.
  * @d: struct dog.
  *
  * Return: no return.
  */
+
+
 void free_dog(dog_t *d)
 {
 if (d == NULL)
@@ -11,7 +17,7 @@ if (d == NULL)
 printf("Error: the dog pointer is NULL.\n");
 return;
 }
-    
+
 free(d->owner);
 free(d->name);
 free(d);
