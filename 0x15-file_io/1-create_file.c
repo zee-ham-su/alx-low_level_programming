baf52acde7be9632d808006caac6b55829d7bef4
zee-ham-su <hsufiian@yahoo.com>
2023-05-08 06:58:34 -0700

    create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * create_file - creates a file with the given text content
+ * @filename: the name of the file to create
+ * @text_content: the text content to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+ssize_t len = 0;
+int fd;
+if (strlen(filename) == 0)
+return (-1);
+
+
+fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (fd == (int) -1)
+{
+return (-1);
+}
+if (strlen(text_content) != 0)
+
+{
+len = write(fd, text_content, strlen(text_content));
+}
+
+close(fd);
+
+return ((len == -1 ? -1 : 1));
+}
