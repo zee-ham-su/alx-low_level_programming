b76c94ec5c07a201f67de589f8e2afc7339a8d46
zee-ham-su <hsufiian@yahoo.com>
2023-10-10 11:55:38 +0000

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,6 +10,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
-
+int binary_search_help(int *array, size_t low, size_t high, int value);
 
 #endif /* SEARCH_ALGOS_H */
